16c07c9818d44b4b404e67112efc6bfa0e953c7f
Natsu <phamdatytb2006@gmail.com>
2026-06-23 12:54:23 +0700

    Day 1

---
+++ src/Day1/ConstantVariable.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class ConstantVariable {
+    public static void main(String[] args) {
+        // Biến không thể thay đổi giá trị sau khi được gán.
+        // dùng từ khóa final
+
+        final int AGE = 20;
+
+       // AGE = 25; bị lỗi
+    }
+}
