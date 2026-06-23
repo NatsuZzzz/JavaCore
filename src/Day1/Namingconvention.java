f35ab9dd7396a9cca7ec80cb594185fa65f7474a
Natsu <phamdatytb2006@gmail.com>
2026-06-23 10:49:35 +0700

    Add JavaCore project

---
+++ src/Day1/Namingconvention.java
@@ -0,0 +1,19 @@
+package Day1;
+import java.util.Scanner;
+
+
+public class NamingConvention {
+    public static void main(String[] args) {
+
+    // có 4 kiểu đặt tên
+        // Variable → camelCase => ví dụ: int isStudent = 18;
+
+        //  Method → camelCase => ví dụ: void twoSum(){}
+
+        //  Class → PascalCase => public class NamingConvention{}
+
+        //  Constant → UPPER_CASE => final double PI = 3.14;
+
+
+    }
+}
---
+++ src/Day1/Variable.java
@@ -0,0 +1,30 @@
+package Day1;
+public class Variable {
+    public static void main(String[] args) {
+
+        // Variable (biến) là một vùng nhớ dùng để lưu trữ dữ liệu.
+
+        int age = 20;
+
+        //int   → kiểu dữ liệu (Data Type)
+        //age   → tên biến (Variable Name)
+        //20    → giá trị (Value)
+
+        // Tại sao cần Variable?
+
+        System.out.println("Boss");
+        System.out.println("Boss");
+        System.out.println("Boss");
+
+        // Nếu muốn đổi tên Boss -> Natsu phải sửa 3 chỗ
+        // dùng biến String
+
+        String name = "Natsu";
+
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+
+        // Chỉ cần sửa 1 lần.
+    }
+}
